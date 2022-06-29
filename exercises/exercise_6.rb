0c17b8a2d49d30b31a6a10a085083d7b9b848c16
Edgarsar <edgarsargsyan@hotmail.com>
2022-06-29 22:05:24 +0000

    Create two employees and store them in relation with @store1 and @store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,5 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Edgar", last_name: "sargsyan", hourly_rate: 40)
